5f10c6d8248bee3a90e1fc186d800c42009811ee
Th bigode <127320304+thzn7@users.noreply.github.com>
2024-08-07 09:27:48 -0300

    ss

--- .js
+++ .js
@@ -2,11 +2,32 @@ const numeroenha = document.querySelector('.parametro=senha__te;')
 const botoes= document.querySelectorAll('.parametro=senha__botao')
 
 botoes[0].onclick = diminuirTamanho
+botoes[1].onclick = aumentarTamanho
 
 let tamanhoSenha = 5
 numeroSenha.textContent = tamanhoSenha
 
+let maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let mainuscula = "abcdefghijkl,mopqrstuvwxyz"
+let numero ="0123456789"
+let simbolos = "!?@#$%&"
+
+console.log(maiuscula);
+
 function diminuirTamanho() {
+    if(tamanhoSenha > 1) {
     tamanhoSenha = tamanhoSenha - 1
     numeroSenha.textContent = tamanhoSenha
+}
+
+function aumentarTamanho(){
+    if(tamanhoSenha < 20)
+        tamanhoSenha = tamanhoSenha + 1
+    numeroSenha.textContent = tamanhoSenha
+}
+
+function geraSenha () {
+    let senha = ""
+    for(let i = 0; i<tamanhoSenha; i++)
+        let numeroAleatorio = Math.random()
 }
